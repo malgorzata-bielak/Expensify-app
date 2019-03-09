f948759702be745548a2286046f7a4ac114ea83e
Malgorzata Bielak <mgw.bielak@gmail.com>
2019-03-09 20:58:44 +0100

    Get firebase expenses on start

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -2,14 +2,14 @@ import moment from "moment";
 
 export default [
   {
-    id: 1,
+    id: "1",
     description: "Gum",
     note: "",
     amount: 195,
     createdAt: 0
   },
   {
-    id: 2,
+    id: "2",
     description: "Rent",
     note: "",
     amount: 109500,
@@ -18,7 +18,7 @@ export default [
       .valueOf()
   },
   {
-    id: 3,
+    id: "3",
     description: "Credit Card",
     note: "",
     amount: 4500,
