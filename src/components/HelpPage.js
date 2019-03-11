97037e4cb56a719dd9ff93fcdd4a61c3652dc8fa
Malgorzata Bielak <mgw.bielak@gmail.com>
2019-03-11 16:46:00 +0100

    Store user data in user area

--- src/components/HelpPage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const HelpPage = () => <div>This is from help compo</div>;
-
-export default HelpPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,11 +4,11 @@ import createHistory from "history/createBrowserHistory";
 import Header from "../components/Header";
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage.js";
 import AddExpensePage from "../components/AddExpensePage.js";
-import HelpPage from "../components/HelpPage.js";
 import EditExpensePage from "../components/EditExpensePage.js";
 import NotFoundPage from "../components/NotFoundPage.js";
 import LoginPage from "../components/LoginPage";
 import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createHistory();
 
@@ -16,11 +16,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
